995cc00d0f9def8bc85f465074b5c4970de8e9a8
eweseongyeoh <eweseongyeoh@airasia.com>
2021-01-05 21:47:56 +0800

    Update HOC usage

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import PosterList from '../../components/PosterList/PosterList'
-import trendingNow from '../../assets/trending-now.json'
-import Original from '../../assets/netflix-originals.json'
-import withNameLogger from '../../components/withNameLogger/withNameLogger'
-import magazines from '../../services/magazines'
+import PosterList from '../../components/PosterList/PosterList';
+import trendingNow from '../../assets/trending-now.json';
+import Original from '../../assets/netflix-originals.json';
+import withNameLogger from '../../components/withNameLogger/withNameLogger';
+import magazines from '../../services/magazines';
+
+const MagazinesWithLoader = withNameLogger(PosterList);
 
 export default class Home extends Component {
-    render () {
-        return (
-            <div>
-                <withNameLogger title='Magazines' load={magazines.getMagazines}/>
-                <PosterList title='Trending Now' posters={trendingNow}/>
-                <PosterList title='Originals' posters={Original}/>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <MagazinesWithLoader title="Magazines" load={magazines.getMagazines} />
+        <PosterList title="Trending Now" posters={trendingNow} />
+        <PosterList title="Originals" posters={Original} />
+      </div>
+    );
+  }
 }
